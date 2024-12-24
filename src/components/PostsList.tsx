504dcde2d958617dbf3eb5f7b115a31060432c19
maxmodrr <maxmodrr@gmail.com>
2024-12-24 03:27:00 +0200

    results

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Post } from '../types/Post';
+import cl from 'classnames';
 
-export const PostsList: React.FC = () => (
-  <div data-cy="PostsList">
-    <p className="title">Posts:</p>
+interface Props {
+  posts: Post[];
+  onActivePost: (v: Post | null) => void;
+  onShowSideBar: (v: boolean) => void;
+}
 
-    <table className="table is-fullwidth is-striped is-hoverable is-narrow">
-      <thead>
-        <tr className="has-background-link-light">
-          <th>#</th>
-          <th>Title</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th> </th>
-        </tr>
-      </thead>
+export const PostsList: React.FC<Props> = ({
+  posts,
+  onActivePost,
+  onShowSideBar,
+}) => {
+  const [activePost, setActivePost] = useState<Post | null>(null);
+  const [showClose, setShowClose] = useState(false);
 
-      <tbody>
-        <tr data-cy="Post">
+  const handleChangeButton = (post: Post) => {
+    if (post.id === activePost?.id) {
+      setShowClose(!showClose);
+      onShowSideBar(showClose ? false : true);
+
+      return;
+    }
+
+    onActivePost(post);
+    onShowSideBar(true);
+
+    setShowClose(true);
+    setActivePost(post);
+  };
+
+  return (
+    <div data-cy="PostsList">
+      <p className="title">Posts:</p>
+
+      <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+        <thead>
+          <tr className="has-background-link-light">
+            <th>#</th>
+            <th>Title</th>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th> </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {posts.map(post => (
+            <tr key={post.id} data-cy="Post">
+              <td data-cy="PostId">{post.id}</td>
+
+              <td data-cy="PostTitle">{post.title}</td>
+
+              <td className="has-text-right is-vcentered">
+                <button
+                  type="button"
+                  data-cy="PostButton"
+                  className={cl('button is-link', {
+                    'is-light': activePost?.id !== post.id || !showClose,
+                  })}
+                  onClick={() => handleChangeButton(post)}
+                >
+                  {showClose && activePost?.id === post.id ? 'Close' : 'Open'}
+                </button>
+              </td>
+            </tr>
+          ))}
+          {/* <tr data-cy="Post">
           <td data-cy="PostId">17</td>
 
           <td data-cy="PostTitle">
             fugit voluptas sed molestias voluptatem provident
-          </td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
           </td>
         </tr>
 
@@ -79,8 +120,9 @@ export const PostsList: React.FC = () => (
               Open
             </button>
           </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+        </tr> */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
